6077ea17a52e05b273bc167607912027c10be634
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-12 22:34:16 -0700

    Sketched basic crud controller action, Checked index page only

--- todo_app/app/controllers/tasks_controller.rb
+++ todo_app/app/controllers/tasks_controller.rb
@@ -1,15 +1,47 @@
 class TasksController < ApplicationController
+
+  before_action :find_task_id, only: [:edit, :update, :show]
+
+
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
+  end
 
+  def create
+    @task = Task.new(task_param)
   end
 
   def edit
+    find_task_id
   end
 
-  def show
+  def update
+    find_task_id
+    if @task.update
+      redirect_to task_path
+    else
+      render 'edit'
+    end
   end
-  
+
+
+  def show
+    find_task_id
+  end
+
+
+
+  private
+
+  def task_param
+    params.require(:task).permit(:title, :detail)
+  end
+
+  def find_task_id
+    @task = Task.find(params[:id])
+  end
 end
