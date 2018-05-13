eb828e60ac824047af3f0ccc398281a821e14c7d
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-12 21:24:59 -0700

    Changed default routes to resorces :task

--- todo_app/config/routes.rb
+++ todo_app/config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'tasks/indexnew'
-
-  get 'tasks/edit'
-
-  get 'tasks/show'
+  resources :tasks
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
