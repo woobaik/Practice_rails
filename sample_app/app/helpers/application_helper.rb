074189b13a1066e034c5de1a6cb86e81e91e997b
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-14 20:52:46 -0700

    Created shorten get for routes, update test accordingly

--- sample_app/app/helpers/application_helper.rb
+++ sample_app/app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def full_title(page_title = "")
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Ruby on Rails Tutorial Sample"
     if page_title.empty?
       base_title
     else
-      page_title + "|" + base_title
-    end 
+      page_title + " | " + base_title
+    end
   end
 
 
