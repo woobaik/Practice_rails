ac41ae9895d33d21f8f1bc2f857d5ea6594af5e6
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-16 16:32:41 -0700

    Create action create for session controller, view, router

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  get 'login', to: 'sessions#new'
+
+  post 'login', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
 
   get 'sign_up', to: 'users#new'
 
@@ -12,6 +16,8 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
 
   get '/contact', to: 'static_pages#contact'
+
+
 
   resources :users
 
