f171e74fdc9bd956f314fee39009b1e68a65989d
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-16 05:29:12 -0700

    Create test for invalid user information sign up

--- sample_app/test/integration/users_signup_test.rb
+++ sample_app/test/integration/users_signup_test.rb
@@ -4,4 +4,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "Invalid user information" do
+    get new_user_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: "",
+                                         email: "user@invaid.com",
+                                         password: "foo",
+                                         password_confirmation: "bar"}}
+    end
+    assert_template 'users/new'
+  end
 end
