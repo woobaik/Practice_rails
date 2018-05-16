01c52b4f7ebfddf9d2d146a5055ec07f93cb103f
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-16 01:22:13 -0700

    Create resource :users, show action for controller, and show.html.erb

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'static_pages#contact'
 
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
