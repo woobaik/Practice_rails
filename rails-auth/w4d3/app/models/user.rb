f630d64002c557404d3ca53652fa00563d0f3fd2
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-19 22:59:00 -0700

    Add index to username, add bulk user data via seeds.rb

--- rails-auth/w4d3/app/models/user.rb
+++ rails-auth/w4d3/app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ApplicationRecord
+
+  def self.how_long_to_find_users_in_ms
+    start = Time.now
+    User.where(username: "haram").to_a
+    (Time.now - start) * 1000
+  end
+
+  def self.find_time_pass
+    start = Time.now
+    User.where(password_digest: "kjsflkjslkdjflksfj" ).to_a
+    (Time.now - start ) * 1000
+  end
+
+  def time
+    Time.now
+
+  end
+
 end
--- rails-auth/w4d3/db/seeds.rb
+++ rails-auth/w4d3/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+1000.times do |input|
+  User.create(username: "haram",
+               password_digest: "kjsflkjslkdjflksfj")
+
+end
