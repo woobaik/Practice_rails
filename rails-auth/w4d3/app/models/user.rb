8771a38addebb543618e75317ca09abe5e81bab9
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-20 21:26:45 -0700

    Create users_controller

--- rails-auth/w4d3/app/models/user.rb
+++ rails-auth/w4d3/app/models/user.rb
@@ -3,14 +3,14 @@ class User < ApplicationRecord
   before_validation :ensure_session_token
 
   validates :username, :session_token, presence: true
-  validates :password_digest, presence: true, { message: "Password, can'be blank!"}
+  validates :password_digest
   validates :password, length: { minumum : 6, allow_nil: true },
 
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return user if user && user.authenticate(password)
-    
+
 
   end
 
--- rails-auth/w4d3/config/routes.rb
+++ rails-auth/w4d3/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'user/index'
+
+  get 'user/new'
+
+  get 'user/create'
+
+  get 'user/show'
+
+  get 'user/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
