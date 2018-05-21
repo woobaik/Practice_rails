00318ca9c78e6292af3a9da3e9a497251dc38458
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-21 03:41:00 -0700

    Try to fix invalid hash error on BCrypt, due to the authenticating upgrate

--- rails-auth/w4d3/app/controllers/sessions_controller.rb
+++ rails-auth/w4d3/app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
   def new
+
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+
+    if user && user.authenticate(params[:session][:password_digest])
+      log_in(user)
+
+      flash[:success] = "You have successfully logged in"
+      redirect_to user_path(user)
+    else
+      flash[:fail] = "You have failed to log in"
+      render 'new'
+    end
+  end
+
+  def destroy
+
+  end
+
 end
--- rails-auth/w4d3/app/models/user.rb
+++ rails-auth/w4d3/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-
+  has_secure_password
 
 
   validates :username, presence: true, length: { minimum: 6 }
