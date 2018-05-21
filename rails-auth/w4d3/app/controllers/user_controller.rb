c92aa774b6f9b291a03d451dcc395936bcdb2183
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-20 21:45:56 -0700

    Create trigger actions for users controller

--- rails-auth/w4d3/app/controllers/user_controller.rb
+++ rails-auth/w4d3/app/controllers/user_controller.rb
@@ -1,16 +1,44 @@
 class UserController < ApplicationController
+
+  before_action :find_user, only: [:edit, :update, :show, :destroy]
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+  end
+
+  def edit
+
+  end
+
+  def update
+
   end
 
   def show
+
   end
 
   def destroy
+
   end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username,
+                                 :password_digest))
+  end
+
+  def find_user
+    @user = User.find_by(id: params[:id])
+  end
+
 end
