b35edcd125a9710eae2193e3f1631d12290db562
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-21 23:16:59 -0700

    Create bookmark page for next day

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -3,7 +3,7 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
-
+ ###start from here
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
 
@@ -16,5 +16,11 @@ module SessionsHelper
   def log_out
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def remember(user)
+    user.remember ##remember method from "user.rb"
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanet[:remember_token] = user.remember_token
   end
 end
