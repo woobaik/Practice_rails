908ed5da77278b7a6d29de6b816ea9e825f2c695
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-30 21:45:52 -0700

    Implement Flash msg, changed order direction for inex

--- alpha_app/app/controllers/articles_controller.rb
+++ alpha_app/app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :find_article, only: [:edit, :update, :show, :destroy]
 
   def index
-    @articles = Article.all
+    @articles = Article.all.order("created_at DESC")
   end
 
   def new
