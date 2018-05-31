1145a477c5337aa1cb79fd71ea28fcbd13ae2352
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-05-31 00:38:02 -0700

    Create controller for pages, added bootstrap on it

--- alpha_app/config/routes.rb
+++ alpha_app/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'pages/about'
+
   resources 'articles'
-  root 'articles#index'
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
