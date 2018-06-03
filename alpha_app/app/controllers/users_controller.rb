7b98a8fb3487c978b2f9ee2865b4a3fcbdb9e995
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-06-03 14:50:46 -0700

    Create User Sign up view

--- alpha_app/app/controllers/users_controller.rb
+++ alpha_app/app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def edit
+    @user = User.new
   end
 end
--- alpha_app/config/routes.rb
+++ alpha_app/config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
 
   resources 'articles'
   root 'pages#home'
+  resources 'users', except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
