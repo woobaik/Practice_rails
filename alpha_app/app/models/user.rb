bfec383e55da5407c898483d5dca44923306bb5f
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-06-03 13:05:40 -0700

    Create password_digest column and validated it

--- alpha_app/app/models/user.rb
+++ alpha_app/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
-  has many :articles
+  has_many :articles
   has_secure_password
+  validates :password_digest, presence: true, length: { minimum: 4 }
+
+  VALID_EMAIL_REGEX = /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}
 end
