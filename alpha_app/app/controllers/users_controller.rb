0cb0f0d7157e4b6620ca6c5b64b91f430bc4df3b
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-06-03 15:08:54 -0700

    Create index new create action for users controller

--- alpha_app/app/controllers/users_controller.rb
+++ alpha_app/app/controllers/users_controller.rb
@@ -7,7 +7,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to Alpha Blog #{@user.username}"
+      redirect_to root_path
+    else
+      flash[:error] = "There Was an Error Signing Up"
+      render 'new'
+    end
+  end
+
   def edit
-    @user = User.new
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+
   end
 end
